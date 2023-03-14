77b03ef35c5fd810560fc142f0dfbf156a17feb0
JayFoxall <46763560+JayFoxall@users.noreply.github.com>
2023-03-14 23:20:22 +0000

    Sounds now working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
-import {Keypad} from 'ComponentLibrary.jsx'
+import {Keypad} from './ComponentLibrary.jsx'
+import { useState } from 'react';
+
 function App() {
-  const buttonsInKeypad =["Q","W","E","A","D","F","Z","X","C"]
+  const [pressedButton, setPressedButton] = useState()
+
+  function KeypadButton(triggerKey, audioClip){
+    this.triggerKey = triggerKey;
+    this.audioClip = audioClip
+  }
+
+  const qButton = new KeypadButton("Q", "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3")
+  const wButton = new KeypadButton("W", "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3")
+  const eButton = new KeypadButton("E", "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3")
+  const aButton = new KeypadButton("A", "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3")
+  const sButton = new KeypadButton("S", "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3")
+  const dButton = new KeypadButton("D", "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3")
+  const zButton = new KeypadButton("Z", "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3")
+  const xButton = new KeypadButton("X", "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3")
+  const cButton = new KeypadButton("C", "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3")
+
+  const buttonsInKeypad = [qButton,wButton,eButton,aButton,sButton,dButton,zButton,xButton,cButton]
   return (
-    <>
-    <Keypad buttons = {buttonsInKeypad} />
-    </>
+    <div id="drum machine">
+      <Keypad buttons = {buttonsInKeypad} />
+      <p id = "display">
+        the display goes here 
+      </p>
+    </div>
   );
 }
 
--- src/ComponentLibrary.jsx
+++ src/ComponentLibrary.jsx
@@ -1,16 +1,53 @@
-import React from "react";  
+import {React} from "react";  
+
 
 export function Button(props){
+
+    function handleClick(event) {
+        let audio = document.getElementById(props.characterDesignation);
+        audio.play();
+    }
+
     return (
-        <>
-            {props.characterDesignation}
-        </>
+        <div className="drum-pad" onClick={handleClick}>
+                {props.characterDesignation}
+                <audio id={props.characterDesignation} class="clip" src={props.audioClip}></audio>
+        </div>
     )
 }
 
 export function Keypad(props){
     let buttons = [...props.buttons]
-    return <>
-    {buttons.map(button => <Button characterDesignation = {button}/>)}
-    </>
+    
+    return( 
+        <>
+            {buttons.map(button => <Button characterDesignation={button.triggerKey} audioClip={button.audioClip}  />)}
+        </>
+    )
 }
+
+// export class Button extends React.Component{
+
+//     constructor(props){
+//         super(props);
+//     }
+
+//     handleClick = (event) => {
+//         let audio = document.getElementById(this.props.characterDesignation);
+//         audio.play();
+//     }
+
+//     render() {
+//         return (
+//             <div className="drum-pad" onClick={this.handleClick}>
+//                     {this.props.characterDesignation}
+//                     <audio id={this.props.characterDesignation} class="clip" src={this.props.audioClip}></audio>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+
+
